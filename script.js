41023c14983d9eaaed2b0b18d5a224571f7a91d3
dhunjadi <74426940+dhunjadi@users.noreply.github.com>
2021-02-22 19:29:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// MODAL START
+modal = document.querySelector('.modal');
+twoPlayers = document.querySelector('.two-players');
+threePlayers = document.querySelector('.three-players');
+fourPlayers = document.querySelector('.four-players');
+
+// TABLE
+table = document.querySelector('.table');
+
+// P1 SQUARE
+p1Square = document.querySelector('.p1-square');
+
+p1_1 = document.querySelector('.p1-1');
+p1_2 = document.querySelector('.p1-2');
+p1_3 = document.querySelector('.p1-3');
+p1_4 = document.querySelector('.p1-4');
+
+// P2 SQUARE
+p2Square = document.querySelector('.p2-square');
+
+p2_1 = document.querySelector('.p2-1');
+p2_2 = document.querySelector('.p2-2');
+p2_3 = document.querySelector('.p2-3');
+p2_4 = document.querySelector('.p2-4');
+
+// P3 SQUARE
+p3Square = document.querySelector('.p3-square');
+
+p3_1 = document.querySelector('.p3-1');
+p3_2 = document.querySelector('.p3-2');
+p3_3 = document.querySelector('.p3-3');
+p3_4 = document.querySelector('.p3-4');
+
+// P4 SQUARE
+p4Square = document.querySelector('.p4-square');
+
+p4_1 = document.querySelector('.p4-1');
+p4_2 = document.querySelector('.p4-2');
+p4_3 = document.querySelector('.p4-3');
+p4_4 = document.querySelector('.p4-4');
+
+p1Length = p1Square.getElementsByTagName('div');
+p2Length = p2Square.getElementsByTagName('div');
+p3Length = p3Square.getElementsByTagName('div');
+p4Length = p4Square.getElementsByTagName('div');
+
+// PLAYER SELECT
+
+gameStart2 = function (){
+  modal.classList.add('hidden');
+  table.classList.remove('hidden');
+  p1Square.classList.remove('hidden');
+  p2Square.classList.remove('hidden');
+  p3Square.classList.remove('hidden');
+  p4Square.classList.remove('hidden');
+  diceDiv.classList.remove('hidden');
+  for(i = 0; i < p3Length.length; i++){
+    p3Length[i].classList.add('disabled');
+    p4Length[i].classList.add('disabled');
+  }
+}
+
+gameStart3 = function (){
+  modal.classList.add('hidden');
+  table.classList.remove('hidden');
+  p1Square.classList.remove('hidden');
+  p2Square.classList.remove('hidden');
+  p3Square.classList.remove('hidden');
+  p4Square.classList.remove('hidden');
+  diceDiv.classList.remove('hidden');
+  for(i = 0; i < p3Length.length; i++){
+    p4Length[i].classList.add('disabled');
+  }
+}
+
+gameStart4 = function (){
+  modal.classList.add('hidden');
+  table.classList.remove('hidden');
+  p1Square.classList.remove('hidden');
+  p2Square.classList.remove('hidden');
+  p3Square.classList.remove('hidden');
+  p4Square.classList.remove('hidden');
+  diceDiv.classList.remove('hidden');
+}
+
+twoPlayers.addEventListener('click', gameStart2);
+threePlayers.addEventListener('click', gameStart3);
+fourPlayers.addEventListener('click', gameStart4);
+
+// DICE ROLL
+
+const diceDiv = document.querySelector('.dice-div');
+const diceImage = document.querySelector('.dice-div img');
+
+diceNumber = function () {
+  diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceImageSource = 'images/dice' + diceNumber + '.png'
+  diceImage.setAttribute('src', diceImageSource);
+}
+
+diceImage.addEventListener('click', diceNumber);
